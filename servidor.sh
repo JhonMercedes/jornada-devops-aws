73bad5f589a3b040a0d06fb094a4f42e726d05ed
JhonMercedes <jhonzera93@gmail.com>
2022-11-09 21:28:54 -0300

    script servidores v1.0

---
+++ servidor.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Infraestrutura como Código - Script de Provisionamento de um Servidor Web (Apache)"
+
+apt update
+apt upgrade -y
+
+echo "Instalando servidor apache2"
+apt install apache2 -y
+
+echo "Instalando pacote unzip"
+apt install unzip -y
+
+echo "Baixando aplicação disponivel GITHUB"
+dpkg -i https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip /tmp
+
+unzip main.zip
+
+echo "Copiando arquivos para dir /apache"
+cp -R linux-site=dio/* /var/wwww/
